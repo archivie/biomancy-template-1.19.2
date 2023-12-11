0cc663c7ae82d3579a3a50fa1d618cb71139ebb7
u7683840 <u7683840@anu.edu.au>
2023-12-11 14:06:37 +1100

    Initial commit

---
+++ src/client/java/net/ren/biomancy/BiomancyClient.java
@@ -0,0 +1,10 @@
+package net.ren.biomancy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BiomancyClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/net/ren/biomancy/BiomancyDataGenerator.java
@@ -0,0 +1,11 @@
+package net.ren.biomancy;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BiomancyDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
